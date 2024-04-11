a52e1224e6a6ddbec579d3c26ee60ff722fe3c8c
PrajapatiVikrant <146848720+PrajapatiVikrant@users.noreply.github.com>
2024-04-11 15:09:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express()
 const PORT = process.env.PUBLIC_PORT || 4000;
 app.use(cors())
+app.get('/',(req,res)=>{res.send('hello world')})
 app.use('/auth',require('./Route/Auth'))
 app.use('/todo',require('./Route/Crud'))
 app.listen(PORT,()=>{
